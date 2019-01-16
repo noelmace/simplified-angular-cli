596cbf95a0c80765fbd5ea3845cbf761bdab3aab
Noel Mace <contact@noelmace.com>
2019-01-16 13:29:06 +0100

    feat: identify a command to run
    Use the first argument passed to sng as the "cli command".
    For now, we just test if it's an valid command and show a message.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log('Hello CLI');
+const sngCommands = ['build'];
+
+function runCommand(arguments: string[]) {
+  if (arguments.length < 1) {
+    throw new Error('Unable to find a command to run');
+  }
+  const commandName: string = arguments.shift()!;
+  if (!sngCommands.includes(commandName)) {
+    return 1;
+  }
+  console.log(`nothing to do with ${commandName} for now`);
+  return 0;
+}
+
+if (process.argv.length < 1) {
+  console.error('use a command');
+  process.exit(1);
+}
+
+try {
+  runCommand(process.argv.slice(2));
+} catch(err) {
+  if (err && err.message) {
+    console.log(err.message);
+  } else {
+    throw err;
+  }
+}
